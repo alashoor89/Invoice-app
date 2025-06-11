a3248cc19303a1d008052e309c7a34d3155c87c5
HASSAN ALASHOOR <37499427+alashoor89@users.noreply.github.com>
2025-06-11 18:58:26 +0300

    - Add guard to check kIsWeb and prevent Platform call - Show screenshot in debugging only in web and windows - Rename login path to /login

--- lib/project/common/widgets/app_screen_shot.dart
+++ lib/project/common/widgets/app_screen_shot.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:path_provider/path_provider.dart';
@@ -12,7 +13,7 @@ class AppScreenShot extends HookWidget {
 
   @override
   Widget build(final BuildContext context) {
-    if (!Platform.isWindows) {
+    if (kReleaseMode || (!kIsWeb && !Platform.isWindows)) {
       return child;
     }
 
